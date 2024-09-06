09bfc4ec95e82ad1dcac810c6a9223f16d4e7cbf
anatonium <67018535+anatonium@users.noreply.github.com>
2024-09-06 17:03:44 +0600

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -5,13 +5,13 @@
 export NVME=/dev/nvme0n1
 export SSD=/dev/sda
 export LUKS=/dev/mapper/luks
-export SWAP_SIZE=24G
+export SWAP_SIZE=8G
 export SWAP=swap
 export ROOT=root
 export VG=rootvg
 export HOSTNAME=earth
-export TIMEZONE=/Europe/Rome
-export USER=matteo
+export TIMEZONE=/Asia/Bishkek
+export USER=anton
 export BOOT_CFG=/boot/loader/entries/arch.conf
 export HOOKS="HOOKS=(base systemd autodetect keyboard sd-vconsole modconf block sd-encrypt sd-lvm2 filesystems fsck)" 
 
